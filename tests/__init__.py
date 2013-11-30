037778eca951558f4a9024af3735510c5b492aee
blaulan <me@blaulan.com>
2013-11-30 22:10:24 +0800

    add tests

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Eric Wu
+# @Date:   2013-11-28 00:17:54
+# @Email:  me@blaulan.com
+# @Last modified by:   Eric Wu
+# @Last Modified time: 2013-11-28 00:17:55
+
